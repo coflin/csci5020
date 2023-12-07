0be3c53d8640291358eb316909221317d12cebab
kaliroti <snehairukuvajjula@gmail.com>
2023-12-07 12:00:33 -0700

    testing

--- final/server.py
+++ final/server.py
@@ -12,18 +12,14 @@ def family_feud_server():
         client, addr = server.accept()
         handle_client(client,addr)
 
-        client.send(b"{console.log(MESSAGE)}")
-
-
 def handle_client(client,addr):
 
     data = client.recv(1024)
     with open('/var/log/family_feud.log', 'a') as log_file:
         log_file.write(f"Received message from {addr}:{data}\n")
 
-    welcome_message = "Welcome to Family Feud!\n Author: Sneha Irukuvajjula\n Created: Dec 7, 2023"
-
-
+    welcome_message = colored("Welcome to Family Feud!\n Author: Sneha Irukuvajjula\n Created: Dec 7, 2023","green")
+    client.send(welcome_message.encode('utf-8'))
 
 if __name__ == "__main__":
     family_feud_server()
