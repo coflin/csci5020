80ba8c09dd18dca66cfdcef10040e8bfdcd17c5d
kaliroti <snehairukuvajjula@gmail.com>
2023-12-07 12:06:52 -0700

    testing

--- final/server.py
+++ final/server.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import socket
-from termcolor import colored
 
 def family_feud_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 5020))
     server.listen(100)
-    print("Family Feud started. Waiting for connection..")
+    print("Family Feud started. Waiting for connections..")
 
     while True:
         client, addr = server.accept()
@@ -18,7 +17,20 @@ def handle_client(client,addr):
     with open('/var/log/family_feud.log', 'a') as log_file:
         log_file.write(f"Received message from {addr}:{data}\n")
 
-    welcome_message = colored("Welcome to Family Feud!\n Author: Sneha Irukuvajjula\n Created: Dec 7, 2023","green")
+    welcome_message = """
+    __        __   _                        _        
+\ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___  
+ \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \ 
+  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) |
+   \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/ 
+                                                     
+ _____               _ _         _____              _ _ 
+|  ___|_ _ _ __ ___ (_) |_   _  |  ___|__ _   _  __| | |
+| |_ / _` | '_ ` _ \| | | | | | | |_ / _ \ | | |/ _` | |
+|  _| (_| | | | | | | | | |_| | |  _|  __/ |_| | (_| |_|
+|_|  \__,_|_| |_| |_|_|_|\__, | |_|  \___|\__,_|\__,_(_)
+                         |___/                          
+"""
     client.send(welcome_message.encode('utf-8'))
 
 if __name__ == "__main__":
