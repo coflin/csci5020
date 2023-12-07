4fc8498f760c10520a5034f7181e8590dfb8d42f
kaliroti <snehairukuvajjula@gmail.com>
2023-12-07 12:46:30 -0700

    testing

--- final/client.py
+++ final/client.py
@@ -5,11 +5,20 @@ def family_feud_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("54.215.236.176", 5020))
 
+    #Welcome message from the server
     welcome_message = client.recv(1024)
     print(welcome_message.decode('utf-8'))
 
-    action = input("1. Create a room\n2. Join a room")
-    client.send(action.encode('utf-8'))
+    #Start Game
+    action = input("1. Create a room\n2. Join a room\n")
+    while action != 1 or action != 2:
+        print("Incorrect option. Try again.")
+        action = input("1. Create a room\n2. Join a room\n")
+    else:
+        client.send(action.encode('utf-8'))
     
-if __name__ == "__main__":
+def main():
     family_feud_client()
+
+if __name__ == "__main__":
+    main()
