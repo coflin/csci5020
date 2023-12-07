5de0f0e2a6ddb71e8bdee86324e89af4e5e20664
kaliroti <snehairukuvajjula@gmail.com>
2023-12-07 12:41:58 -0700

    testing

--- final/client.py
+++ final/client.py
@@ -2,10 +2,8 @@ import socket
 
 def family_feud_client():
 
-    #MESSAGE=b"Hellooooo!"
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("54.215.236.176", 5020))
-    #client.send(MESSAGE)
 
     welcome_message = client.recv(1024)
     print(welcome_message.decode('utf-8'))
