40b03a96f9a9534be0e10a50cae3af8571aa5c7f
kaliroti <snehairukuvajjula@gmail.com>
2023-12-08 13:31:59 -0700

    testing

--- final/server.py
+++ final/server.py
@@ -1,58 +1,72 @@
-#!/usr/bin/python3
-
 import socket
-import concurrent.futures
+import selectors
+import types
 from loguru import logger
 
-logger.add("/var/log/family_feud_server.log")
+sel = selectors.DefaultSelector()
 
-def family_feud_server(server):
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        while True:
-            client, addr = server.accept()
-            executor.submit(handle_client, client, addr)
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
 
-def handle_client(client,addr):
-    try:
-        welcome_message = "\033[92m" + """ 
-__        __   _                            _        
-\ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___  
- \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \ 
-  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) |
-   \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/ 
-                                                     
- ____             _             _____              _ _ 
-/ ___| _ __   ___| |__   __ _  |  ___|__ _   _  __| | |
-\___ \| '_ \ / _ \ '_ \ / _` | | |_ / _ \ | | |/ _` | |
- ___) | | | |  __/ | | | (_| | |  _|  __/ |_| | (_| |_|
-|____/|_| |_|\___|_| |_|\__,_| |_|  \___|\__,_|\__,_(_)    
+    # Welcome Message
+    welcome_message = "\033[92m" + """ 
+    __        __   _                            _        
+    \ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___  
+     \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \ 
+      \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) |
+       \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/ 
+                                                         
+     ____             _             _____              _ _ 
+    / ___| _ __   ___| |__   __ _  |  ___|__ _   _  __| | |
+    \___ \| '_ \ / _ \ '_ \ / _` | | |_ / _ \ | | |/ _` | |
+     ___) | | | |  __/ | | | (_| | |  _|  __/ |_| | (_| |_|
+    |____/|_| |_|\___|_| |_|\__,_| |_|  \___|\__,_|\__,_(_)
+    """ + "\033[0m" + "What is your name? "
+    conn.sendall(welcome_message.encode('utf-8'))
 
-""" + "\033[0m" + "What is your name? "
-        client.sendall(welcome_message.encode('utf-8'))
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
 
-        username = client.recv(1024)
-        logger.info(f" Player {addr}: {username.decode('utf-8')}")
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
 
-        hello_message = b"Hello! Do you want to create a room or join a room? Type 'create or join'"
-        client.sendall(hello_message)
-
-    # while True:
-    #     action = client.recv(1024)
-    #     if not action:
-    #         break  # Break the loop if the client disconnects
-    #     action_message = b"Ok " + action + "ing"
-    #     client.sendall(action_message)
-    except Exception as e:
-        logger.error(f"Exception in handle_client for {addr}: {e}")
-  
-
-@logger.catch
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 5020))
-    server.listen(100)
+    server.listen()
     print("Family Feud started. Waiting for connections..")
-    family_feud_server(server)
+    server.setblocking(False)
+    sel.register(server, selectors.EVENT_READ, data=None)
+
+    try:
+        while True:
+            events = sel.select()
+            for key, mask in events:
+                if key.data is None:
+                    accept_wrapper(key.fileobj)
+                else:
+                    service_connection(key, mask)
+    except KeyboardInterrupt:
+        print("Caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
 
 if __name__ == "__main__":
     main()
