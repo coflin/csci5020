c0371e2e7a5c421a6ea9a33282a64b83c22886f8
kaliroti <snehairukuvajjula@gmail.com>
2023-12-08 12:17:30 -0700

    testing

--- final/server.py
+++ final/server.py
@@ -4,20 +4,13 @@ from loguru import logger
 
 logger.add("/var/log/family_feud_server.log")
 
-def family_feud_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', 5020))
-    server.listen(100)
-    print("Family Feud started. Waiting for connections..")
+def family_feud_server(server):
 
     while True:
         client, addr = server.accept()
         handle_client(client, addr)
 
 def handle_client(client,addr):
-
-    # data = client.recv(1024)
-    # logger.info(f"Received message from {addr}:{data}\n")
 
     welcome_message = "\033[92m" + """ 
 __        __   _                            _        
@@ -32,16 +25,21 @@ __        __   _                            _
 |  _| (_| | | | | | | | | |_| | |  _|  __/ |_| | (_| |_|
 |_|  \__,_|_| |_| |_|_|_|\__, | |_|  \___|\__,_|\__,_(_)
                          |___/                          
-""" + "\033[0m"
-    client.send(welcome_message.encode('utf-8'))
+""" + "\033[0m" + "What is your name?"
+    client.sendall(welcome_message.encode('utf-8'))
 
-    action_response = client.recv(1024)
-    logger.info(action_response.decode("utf-8"))
+    username = client.recv(1024).decode("utf-8")
+    logger.info(f" Player 1: {username}")
 
+    client.sendall(f"Hello {username}")
 
 @logger.catch
 def main():
-    family_feud_server()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 5020))
+    server.listen(100)
+    print("Family Feud started. Waiting for connections..")
+    family_feud_server(server)
 
 if __name__ == "__main__":
     family_feud_server()
