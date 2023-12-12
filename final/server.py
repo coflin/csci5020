c5e61b09686a1d37c78acde4cb1bd76b0e15f490
kaliroti <snehairukuvajjula@gmail.com>
2023-12-12 00:46:09 -0700

    testing

--- final/server.py
+++ final/server.py
@@ -8,7 +8,7 @@ logger.add("/var/log/family_feud_server.log")
 
 questions = ["Question 1", "Question 2", "Question 3"]
 current_question_index = 0
-start_game_event = threading.Event()
+#start_game_event = threading.Event()
 
 players = []
 
@@ -27,9 +27,8 @@ def countdown(client_socket):
         client_socket.sendall(str(i).encode('utf-8'))
 
     # Notify that the countdown is over
-    start_game_event.set()
+    #start_game_event.set()
 
-lock = threading.Lock()
 def handle_client(client_socket, client_id):
     # Send welcome message
     welcome_message = """\033[92m
@@ -78,9 +77,8 @@ __        __   _                            _
     logger.info(f"creator:{creator}\nplayer2:{player2}")
 
         # Notify the creator to start the game
-    creator["socket"].sendall("Type 'start' to begin the game: ".encode('utf-8'))
-
     player2["socket"].sendall("Waiting for the creator to start the game")
+    creator["socket"].sendall("Type 'start' to begin the game: ".encode('utf-8'))
     start_game_response = creator["socket"].recv(1024).decode('utf-8').strip().lower()
 
 
@@ -129,7 +127,7 @@ __        __   _                            _
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 5020))
-    server.listen(2)  # Listen for up to 20 connections
+    server.listen(4)  # Listen for up to 20 connections
 
     print("Family Feud server started. Waiting for connections...")
 
@@ -147,7 +145,7 @@ def main():
 
     except Exception as e:
         print(f"Error accepting connections: {e}")
-        
+
     finally:
         server.close()
 
