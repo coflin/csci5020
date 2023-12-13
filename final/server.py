eec161d5d70acfdc2c428eff36943d1d3e5dc180
kaliroti <snehairukuvajjula@gmail.com>
2023-12-12 19:26:40 -0700

    testing

--- final/server.py
+++ final/server.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-import threading
 import time
 import sqlite3
 import random
@@ -9,38 +8,26 @@ from loguru import logger
 logger.add("/var/log/family_feud_server.log")
 
 scores_lock = threading.Lock()
-scores_updated_event = threading.Event()
 
-def check_winner(username, player_scores):
-    logger.info(f"PLAYER SCORES: {player_scores}")
-    player1_score = None
-    player2_score = None
-    for player in player_scores:
-        if username == list(player.keys())[0]:
-            player1_score = player
-        else:
-            player2_score = player
+def check_winner(username, player1_scores, player2_scores):
+    logger.info(f"PLAYER 1 SCORE: {player1_scores.get(username, 0)}")
+    logger.info(f"PLAYER 2 SCORE: {player2_scores.get(username, 0)}")
 
-    if player1_score is not None and player2_score is not None:
-        score1 = player1_score.get(username, 0)
-        score2 = player2_score.get(username, 0)
+    score1 = player1_scores.get(username, 0)
+    score2 = player2_scores.get(username, 0)
 
-        with scores_lock:
-            if score1 > score2:
-                logger.info(f"PLAYER 1 WON {player1_score[username]}")
-                return player1_score[username]
-            elif score1 < score2:
-                logger.info(f"PLAYER 2 WON {player2_score[username]}")
-                return player2_score[username]
-            else:
-                logger.info(f"NOBODY WON {player1_score[username]}")
-                return None
+    if score1 > score2:
+        logger.info(f"PLAYER 1 WON {score1}")
+        return score1
+    elif score1 < score2:
+        logger.info(f"PLAYER 2 WON {score2}")
+        return score2
     else:
+        logger.info(f"NOBODY WON {score1}")
         return None
-    
-def handle_client(client_socket,clients,barrier):
-    try:
         
+def handle_client(client_socket, clients, barrier, player1_scores, player2_scores):
+    try:
         # Send a welcome message
         client_socket.send(b"Enter your username: ")
         
@@ -50,62 +37,54 @@ def handle_client(client_socket,clients,barrier):
         # Receive and print the client's name
         username = client_socket.recv(1024).decode("utf-8").strip()
         logger.info(f"Client {username} connected.")
-        player_scores={}
 
-        # Add the client to the list
-        clients.append((client_socket, username))
-        player_scores = [{username: 0} for _, username in clients]
-        logger.info(f"CLIENT INFO:{player_scores}")
-
-        # Wait for 2 players to join
+        # Wait for players to join
         barrier.wait()
 
         # Send a starting game message
         client_socket.send(b"Starting game in\n")
-        for i in range(3,0,-1):
+        for i in range(3, 0, -1):
             time.sleep(1)
             client_socket.send(f"{i}..\n".encode("utf-8"))
 
         # Get a random question and send it to the client
-        for question_number in range(1,6):
+        for question_number in range(1, 6):
             question = get_random_question(used_questions)
             client_socket.send(f"\033[1mQuestion {question_number}: {question['prompt']}\033[0m\n".encode("utf-8"))
-                    
+
             # Simulate receiving the client's response
             time.sleep(1)
-            for i in range(1,6):
+            for i in range(1, 6):
                 client_socket.send(f"\033[93mGuess {i}:\033[0m ".encode("utf-8"))
                 guess = client_socket.recv(1024).decode("utf-8")
                 guesses.append(guess)
-            logger.info(f"Client {username} answered: {guesses}")
 
-            logger.info(f"{player_scores}")
-            question_score = calculate_score(question,guesses)
-            with scores_lock:
-                for player_score in player_scores:
-                    if username == list(player_score.keys())[0]:
-                        player_score[username] += question_score
-            client_socket.send(f"\033[92mYour score for this question is: {question_score}\033[0m\n".encode("utf-8"))
-            time.sleep(1)
-            client_socket.send(f"\033[93mYour score so far: {player_score[username]}\033[0m\n\n".encode("utf-8"))
-            time.sleep(1)
-            
+            logger.info(f"Client {username} answered: {guesses}")
+            logger.info(f"Player 1 Scores: {player1_scores}")
+            logger.info(f"Player 2 Scores: {player2_scores}")
+
+            question_score = calculate_score(question, guesses)
+
+            # Update the respective player's score
+            if username in player1_scores:
+                player1_scores[username] += question_score
+                client_socket.send(f"\033[93mYour score so far: {player1_scores[username]}\033[0m\n\n".encode("utf-8"))
+            elif username in player2_scores:
+                player2_scores[username] += question_score
+                client_socket.send(f"\033[93mYour score so far: {player2_scores[username]}\033[0m\n\n".encode("utf-8"))
+
+            time.sleep(1)      
+                  
             # Wait for all players to finish before moving to the next question
             barrier.wait()
 
-        # Wait for all players to finish updating scores before checking the winner
-        scores_updated_event.wait()
-
-        winner = check_winner(username, player_scores)
+        winner = check_winner(username, player1_scores, player2_scores)
         if winner:
             for client_info in clients:
                 client_info[0].send(f"{winner.upper()} WINS!".encode("utf-8"))
         else:
             for client_info in clients:
                 client_info[0].send(f"IT'S A TIE!".encode("utf-8"))
-        
-        # Reset the event for the next round
-        scores_updated_event.clear()
 
     except Exception as e:
         import traceback
@@ -116,10 +95,10 @@ def handle_client(client_socket,clients,barrier):
         # Close the client socket
         client_socket.close()
 
-def calculate_score(question,guesses):
+def calculate_score(question, guesses):
     score = 0
     for guess in guesses:
-        for i in range(1,6):
+        for i in range(1, 6):
             if guess.strip().lower() == question[f'guess{i}']:
                 logger.info(question[f'guess{i}'])
                 score += question[f'guess{i}_score']
@@ -130,7 +109,7 @@ def get_random_question(used_questions):
     """Gets and returns a random question that has not been used before"""
     conn = sqlite3.connect("questions.db")
     cursor = conn.cursor()
-    logger.info("connected to database")
+    logger.info("connected to the database")
 
     # Get all questions
     cursor.execute("SELECT * FROM questions;")
@@ -177,7 +156,8 @@ def main():
     logger.info(f"Server listening on {SERVER_IP}:{SERVER_PORT}")
     
     clients = []
-    
+    player1_scores = {}
+    player2_scores = {}
 
     try:
         # Accept incoming connections
@@ -186,14 +166,14 @@ def main():
             print(f"Accepted connection from {address}")
 
             # Start a new thread to handle the client
-            threading.Thread(target=handle_client, args=(client,clients,barrier)).start()
+            threading.Thread(target=handle_client, args=(client, clients, barrier, player1_scores, player2_scores)).start()
 
             # If two clients have connected, reset the barrier for the next round
             if len(clients) == 2:
                 barrier.reset()
 
     except Exception as e:
-        print(f"Error in main loop: {e}")
+        print(f"Error in the main loop: {e}")
 
     finally:
         server_socket.close()
