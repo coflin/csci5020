f130e5484c79a721a5292e8147e5324671fb41cc
kaliroti <snehairukuvajjula@gmail.com>
2023-12-14 13:59:03 -0700

    testing

--- final/questions.py
+++ final/questions.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 import sqlite3
-import os
 
-DB_FILE = "questions.db"
+"""
+Creates a questions.db database and adds questions to it. 
 
-def create_database():
+Format: 
+{
+    "prompt": "Question1",
+    "guesses": [
+        {"guess": "guess 1", "score": 62},
+        {"guess": "guess 2", "score": 50},
+        {"guess": "guess 3", "score": 12},
+        {"guess": "guess 4", "score": 5},
+        {"guess": "guess 5", "score": 4},
+    ]
+}
+
+"""
+
+
+def create_database(db_file):
     """Create the SQLite database and the necessary table."""
-    conn = sqlite3.connect(DB_FILE)
+    conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
 
     # Create the questions table if it doesn't exist
@@ -30,8 +45,8 @@ def create_database():
     conn.commit()
     conn.close()
 
-def insert_question(prompt, guesses):
-    conn = sqlite3.connect(DB_FILE)
+def insert_question(db_file, prompt, guesses):
+    conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
 
     # Generate the SQL query for inserting a new question
@@ -49,62 +64,69 @@ def insert_question(prompt, guesses):
     conn.commit()
     conn.close()
 
-# Check if the database file exists, create it if not
-create_database()
+def main():
+    # Define the database file
+    db_file = "questions.db"
 
-new_questions = [
-{
-    "prompt": "Name something a movie hero will see in a haunted house.",
-    "guesses": [
-        {"guess": "ghosts", "score": 41},
-        {"guess": "spiders", "score": 14},
-        {"guess": "dead", "score": 13},
-        {"guess": "skeletons", "score": 6},
-        {"guess": "blood", "score": 5},
-    ]
-},
-{
-    "prompt": "Name a way an egg could be prepared that also describes your boss.",
-    "guesses": [
-        {"guess": "scrambled", "score": 39},
-        {"guess": "fried", "score": 26},
-        {"guess": "sunny-side up", "score": 14},
-        {"guess": "runny", "score": 12},
-        {"guess": "over easy", "score": 9},
-    ]
-},
-{
-    "prompt": "Name someone the dog complains about to the pet psychologist.",
-    "guesses": [
-        {"guess": "cat", "score": 35},
-        {"guess": "owner", "score": 30},
-        {"guess": "mailman", "score": 10},
-        {"guess": "kids", "score": 7},
-        {"guess": "vet", "score": 7},
-    ]
-},
-{
-    "prompt": "Name a reason you cover your mouth.",
-    "guesses": [
-        {"guess": "cough", "score": 69},
-        {"guess": "sneeze", "score": 50},
-        {"guess": "yawn", "score": 12},
-        {"guess": "yellow teeth", "score": 5},
-        {"guess": "shock", "score": 4},
-    ]
-},
-{
-    "prompt": "Name something you bought and theen suffered buyer's remorse.",
-    "guesses": [
-        {"guess": "car", "score": 48},
-        {"guess": "clothes", "score": 20},
-        {"guess": "house", "score": 7},
-        {"guess": "food", "score": 3},
-        {"guess": "pet", "score": 1},
-    ]
-}
-]
+    # Check if the database file exists, create it if not
+    create_database(db_file)
 
-# Insert the new questions into the database
-for question in new_questions:
-    insert_question(question["prompt"], question["guesses"])
+    new_questions = [
+        {
+            "prompt": "Name something a movie hero will see in a haunted house.",
+            "guesses": [
+                {"guess": "ghosts", "score": 41},
+                {"guess": "spiders", "score": 14},
+                {"guess": "dead", "score": 13},
+                {"guess": "skeletons", "score": 6},
+                {"guess": "blood", "score": 5},
+            ]
+        },
+        {
+            "prompt": "Name a way an egg could be prepared that also describes your boss.",
+            "guesses": [
+                {"guess": "scrambled", "score": 39},
+                {"guess": "fried", "score": 26},
+                {"guess": "sunny-side up", "score": 14},
+                {"guess": "runny", "score": 12},
+                {"guess": "over easy", "score": 9},
+            ]
+        },
+        {
+            "prompt": "Name someone the dog complains about to the pet psychologist.",
+            "guesses": [
+                {"guess": "cat", "score": 35},
+                {"guess": "owner", "score": 30},
+                {"guess": "mailman", "score": 10},
+                {"guess": "kids", "score": 7},
+                {"guess": "vet", "score": 7},
+            ]
+        },
+        {
+            "prompt": "Name a reason you cover your mouth.",
+            "guesses": [
+                {"guess": "cough", "score": 69},
+                {"guess": "sneeze", "score": 50},
+                {"guess": "yawn", "score": 12},
+                {"guess": "yellow teeth", "score": 5},
+                {"guess": "shock", "score": 4},
+            ]
+        },
+        {
+            "prompt": "Name something you bought and then suffered buyer's remorse.",
+            "guesses": [
+                {"guess": "car", "score": 48},
+                {"guess": "clothes", "score": 20},
+                {"guess": "house", "score": 7},
+                {"guess": "food", "score": 3},
+                {"guess": "pet", "score": 1},
+            ]
+        }
+    ]
+
+    # Insert the new questions into the database
+    for question in new_questions:
+        insert_question(db_file, question["prompt"], question["guesses"])
+
+if __name__ == "__main__":
+    main()
--- final/server.py
+++ final/server.py
@@ -4,19 +4,28 @@ import time
 import sqlite3
 from loguru import logger
 
-"""
-Same question to both parties sequentially. Possibly have 2 tables so that the questions don't repeat.
-"""
-
 logger.add("/var/log/family_feud_server.log")
 
+# Use threading.Lock to synchronize access to shared resources
 scores_lock = threading.Lock()
 
-def handle_client(client_socket,clients,barrier):
-    try:
 
+def handle_client(client_socket, clients, barrier):
+    """
+    Handles communication with a connected client.
+
+    Args:
+        client_socket (socket.socket): The socket object for the client.
+        clients (list): List of connected clients.
+        barrier (threading.Barrier): Synchronization barrier.
+
+    Returns:
+        None
+    """
+    try:
         # Send a welcome message
-        client_socket.send(b"""\033[93m
+        client_socket.send(
+            b"""\033[93m
 __          __  _                            _        
 \ \        / / | |                          | |       
  \ \  /\  / /__| | ___ ___  _ __ ___   ___  | |_ ___  
@@ -62,18 +71,19 @@ The most popular answer earns the highest points, followed by decreasing points 
 \n\033[93m\033[1mWinning \033[0m\033[0m \n
 The player with the highest total points at the end of the game wins! Have fun!
 \n\033[93m\033[1mLet's play Sneha's Family Feud!\033[0m\033[0m
-""")
+"""
+        )
 
         time.sleep(1)
         client_socket.send(b"\n\n Enter your name: ")
-        
+
         used_questions = []
         guesses = []
 
         # Receive and print the client's name
         username = client_socket.recv(1024).decode("utf-8").strip()
         logger.info(f"Client {username} connected.")
-        
+
         # Add the client to the list
         clients.append((client_socket, username))
         player_scores = [{username: 0} for _, username in clients]
@@ -82,68 +92,112 @@ The player with the highest total points at the end of the game wins! Have fun!
         # Wait for 2 players to join
         client_socket.send(b"Waiting for another player to join..\n")
         barrier.wait()
-    
+
         # Send a starting game message
         client_socket.send(b"All players have joined. Starting game in\n")
-        for i in range(3,0,-1):
+        for i in range(3, 0, -1):
             time.sleep(1)
             client_socket.send(f"{i}..\n".encode("utf-8"))
 
         # Get a random question and send it to the client
-        for question_number in range(1,6):
+        for question_number in range(1, 6):
             question = get_next_question(used_questions)
-            client_socket.send(f"\n\n\033[37m\033[1mQuestion {question_number}: {question['prompt']}\033[0m\033[0m".encode("utf-8"))
-                    
+            client_socket.send(
+                f"\n\n\033[37m\033[1mQuestion {question_number}: {question['prompt']}\033[0m\033[0m".encode(
+                    "utf-8"
+                )
+            )
+
             # Simulate receiving the client's response
             time.sleep(1)
-            for i in range(1,6):
+
+            # Set a 30-second time limit for the user to answer
+            time_limit = 30  # in seconds
+            start_time = time.time()
+
+            for i in range(1, 6):
                 client_socket.send(f"\n\033[93mGuess {i}:\033[0m ".encode("utf-8"))
                 guess = client_socket.recv(1024).decode("utf-8")
+
+                if time.time() - start_time > time_limit:
+                    client_socket.send(b"\033[91mTime's up! Moving to the next question.\033[0m\n")
+                    break
+                
                 guesses.append(guess)
 
             logger.info(f"Client {username} answered: {guesses}")
             logger.info(f"{player_scores}")
 
-            question_score = calculate_score(question,guesses)
+            question_score = calculate_score(question, guesses)
             with scores_lock:
                 for player_score in player_scores:
                     if username == list(player_score.keys())[0]:
                         player_score[username] += question_score
-            client_socket.send(f"\033[92mYour score for this question is: {question_score}\033[0m\n".encode("utf-8"))
+            client_socket.send(
+                f"\033[92mYour score for this question is: {question_score}\033[0m\n".encode(
+                    "utf-8"
+                )
+            )
             time.sleep(1)
-            client_socket.send(f"\033[93mYour total score so far: {player_score[username]}\033[0m\n\n".encode("utf-8"))
+            client_socket.send(
+                f"\033[93mYour total score so far: {player_score[username]}\033[0m\n\n".encode(
+                    "utf-8"
+                )
+            )
             time.sleep(1)
-            
+
             # Wait for all players to finish before moving to the next question
             barrier.wait()
 
         final_score = print_in_box(f"Your final score: {player_score[username]}")
-                     
+
         client_socket.send(f"{final_score}\n\n".encode("utf-8"))
         time.sleep(1)
-        client_socket.send(b"Thanks for playing this game. Good bye! :) ")        
+        client_socket.send(b"Thanks for playing this game. Good bye! :) ")
 
     except Exception as e:
         import traceback
+
         traceback.print_exc()
-        logger.error(f"Error handling client: {e}")    
+        logger.error(f"Error handling client: {e}")
 
     finally:
         # Close the client socket
         client_socket.close()
 
-def calculate_score(question,guesses):
+
+def calculate_score(question, guesses):
+    """
+    Calculates the score for a given question and set of guesses.
+
+    Args:
+        question (dict): The question data from the database.
+        guesses (list): List of guesses provided by the player.
+
+    Returns:
+        int: The calculated score for the question.
+    """
     score = 0
     for guess in guesses:
-        for i in range(1,6):
-            if guess.strip().lower() == question[f'guess{i}']:
-                logger.info(question[f'guess{i}'])
-                score += question[f'guess{i}_score']
+        for i in range(1, 6):
+            if guess.strip().lower() == question[f"guess{i}"]:
+                logger.info(question[f"guess{i}"])
+                score += question[f"guess{i}_score"]
     logger.info(f"SCORE: {score}")
     return score
 
+
 def get_next_question(used_questions):
-    """Gets and returns a random question that has not been used before"""
+    """
+    Gets the next question sequentially from the database.
+
+    Args:
+        used_questions (list): List of questions already used.
+
+    Returns:
+        dict: The next question from the database.
+    """
+
     conn = sqlite3.connect("questions.db")
     cursor = conn.cursor()
     logger.info("connected to database")
@@ -160,7 +214,9 @@ def get_next_question(used_questions):
         questions_dict_list.append(question_dict)
 
     # Filter out used questions
-    available_questions = [q for q in questions_dict_list if q["prompt"] not in used_questions]
+    available_questions = [
+        q for q in questions_dict_list if q["prompt"] not in used_questions
+    ]
 
     if not available_questions:
         # Reset used questions if all have been used
@@ -176,19 +232,32 @@ def get_next_question(used_questions):
     conn.close()
     return question
 
+
 def print_in_box(text):
+    """
+    Formats a message in a styled box.
+
+    Args:
+        text (str): The text message to be formatted.
+
+    Returns:
+        str: The formatted message in a box.
+    """
     box_width = len(text) + 4  # Adjust the width based on the length of the text
-    horizontal_line = '+' + '-' * (box_width - 2) + '+'
-    box_string = f'\033[1;92m{horizontal_line}\n| {text} |\n{horizontal_line}\033[0m'    
+    horizontal_line = "+" + "-" * (box_width - 2) + "+"
+    box_string = f"\033[1;92m{horizontal_line}\n| {text} |\n{horizontal_line}\033[0m"
     return box_string
 
+
 barrier = threading.Barrier(2)
+
+
 @logger.catch()
 def main():
     # Create a server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    SERVER_IP = '0.0.0.0'
+    SERVER_IP = "0.0.0.0"
     SERVER_PORT = 5020
 
     # Bind the server socket to the IP and port
@@ -197,9 +266,8 @@ def main():
     # Listen for incoming connections
     server_socket.listen(2)
     logger.info(f"Server listening on {SERVER_IP}:{SERVER_PORT}")
-    
+
     clients = []
-    
 
     try:
         # Accept incoming connections
@@ -208,7 +276,9 @@ def main():
             print(f"Accepted connection from {address}")
 
             # Start a new thread to handle the client
-            threading.Thread(target=handle_client, args=(client,clients,barrier)).start()
+            threading.Thread(
+                target=handle_client, args=(client, clients, barrier)
+            ).start()
 
             # If two clients have connected, reset the barrier for the next round
             if len(clients) == 2:
@@ -219,6 +289,7 @@ def main():
 
     finally:
         server_socket.close()
+
 
 if __name__ == "__main__":
     main()
